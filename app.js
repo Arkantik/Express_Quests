ea587465ee0767d76fe5217e6000e9d4ab06ed58
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-08 13:41:08 +0200

    Challenge of express 3 quest

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -15,10 +16,14 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+app.post("/api/movies", movieHandlers.postMovie);
+
 const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/users", usersHandlers.postUsers);
 
 app.listen(port, (err) => {
 	if (err) {
