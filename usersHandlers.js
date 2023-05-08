ee948547f742bae43aa8c4b9cf7d856b31d65ef3
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-08 13:52:51 +0200

    Challenge of Express 4 quest

--- usersHandlers.js
+++ usersHandlers.js
@@ -30,7 +30,7 @@ const getUsersById = (req, res) => {
 		});
 };
 
-const postUsers = (req, res) => {
+const postUser = (req, res) => {
 	const { firstname, lastname, email, city, language } = req.body;
 
 	database
@@ -48,8 +48,31 @@ const postUsers = (req, res) => {
 		});
 };
 
+const updateUser = (req, res) => {
+	const id = parseInt(req.params.id);
+	const { firstname, lastname, email, city, language } = req.body;
+
+	database
+		.query(
+			"update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+			[firstname, lastname, email, city, language, id]
+		)
+		.then(([result]) => {
+			if (result.affectedRows === 0) {
+				res.status(404).send("Not Found");
+			} else {
+				res.sendStatus(204);
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send("Error saving the user");
+		});
+};
+
 module.exports = {
 	getUsers,
 	getUsersById,
-	postUsers,
+	postUser,
+	updateUser,
 };
